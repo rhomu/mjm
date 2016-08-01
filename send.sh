b06054789917a4a1908d732e9735f06545e076c1
Romain Mueller <romain.mueller@gmail.com>
2016-08-01 21:45:13 +0100

    changing names

--- send.sh
+++ send.sh
@@ -2,32 +2,63 @@
 
 # Send job using screen. Usage:
 #
-#    ./send_job name command
+#    ./send_job command
+#
+# or
+#
+#    ./send_job -n name command
 #
 # The screen session is killed after the command returns. EXPLAIN THE NODE STORY. The actual screen session name is
 #
 #    mjm.name.i
 #
-# where i is the smallest job number aviable (starting from 0).
+# where i is the smallest job number available (starting from 0). If no name is provided then .name is omited.
 
 # TBI
 #  * priority/queue
 #  * logging, using tee
 
+# get job name from options
+NAME=""
+while getopts ":n:" opt; do
+  case $opt in
+    n)
+      NAME=".$OPTARG" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
 # construct job name...
 # ...get all numbers of sub-jobs (i.e. name followed by .number)
 read -a PIDS <<< $( screen -ls \
-                    | awk "/mjm\.$1[.0-9]*\t/ {print \$1}" \
+                    | awk "/mjm${NAME}[.0-9]*\t/ {print \$1}" \
                     | sed "s/.*\.//g"
-                    )
+                  )
 # ...find smallest number not in this list
 for (( i=0 ;; ++i )); do
   if ! echo " ${PIDS[*]} " | grep " $i " -q ; then
     # ...and add it to jobname
-    JNAME="mjm.$1.$i"
+    JNAME="mjm${NAME}.$i"
     break
   fi
 done
+
+# get the command...
+if [ -z ${NAME} ] ; then
+  CMD="${@}"
+else
+  CMD="${@:3}"
+fi
+# ... and a timestamp
+TS=$(date +%s)
 
 # send job...
 echo "Sending job $JNAME ... "
@@ -38,6 +69,6 @@ JNAME_ESCAPE=$(echo "$JNAME" | sed 's/\./\\\./g')
 # ...get PID back (there are sometimes conflicts)
 PID=$(screen -ls | awk "/\.${JNAME_ESCAPE}\t/ {print strtonum(\$1)}")
 # ...send the command to the screen session
-screen -S "$PID.$JNAME" -p 0 -X stuff "$2 ; exit$(printf \\r)"
+screen -S "$PID.$JNAME" -p 0 -X stuff "sleep 1 ; ( ${CMD} | tee ${JNAME}.${TS}.out ) ; exit$(printf \\r)"
 # ...wait a bit
 sleep 0.3
--- wait.sh
+++ wait.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Wait for the number of jobs in a node to be less than a certain number. Usage
+# Wait for the number of jobs in a node to be less than a certain number. Usage:
 #
 #    ./wait name N
 #
-# Checks every second that the number of subprocesses running on node name is greater than N. If this is not the case, returns.
+# Checks every 5 seconds that the number of subprocesses running on node name is greater than N. If this is not the case, returns.
 
-# if timer is set to 0
+# if N is set to 0
 if [ $2 == "0" ]; then
   exit 0;
 fi
 
 # Get all sub jobs with that name (i.e. name followed by .number)
-read -a PIDS <<< $( ./list_jobs.sh )
+read -a PIDS <<< $( ${MJM_PATH}/list.sh )
 
 while [ ${#PIDS[@]} -ge $2 ]; do
-  sleep 1
-  read -a PIDS <<< $( ./list_jobs.sh )
+  sleep 5
+  read -a PIDS <<< $( ${MJM_PATH}/list.sh )
 done
