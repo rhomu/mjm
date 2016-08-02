fbdc95d8953aa3455bdf98e22441f35de8fe7585
Romain Mueller <romain.mueller@gmail.com>
2016-08-02 08:55:58 +0100

    adding unnamed jobs

--- list.sh
+++ list.sh
@@ -5,8 +5,15 @@
 #    ./list_jobs name
 #
 
-# Get all sub jobs with that name (i.e. name followed by .number)
-read -a PIDS <<< $(screen -ls | awk "/mjm\.$1.*\t/ {print \$1}")
+# get name
+if [ -z $1 ] ; then
+  NAME="$1"
+else
+  NAME=".$1"
+fi
+
+# get all sub jobs with that name (i.e. name followed by .number)
+read -a PIDS <<< $(screen -ls | awk "/mjm${NAME}.*\t/ {print \$1}")
 
 # print it
 echo "${PIDS[*]}"
