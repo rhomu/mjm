c47366c3e059b21b25a4beb374a66a6796277d4b
Romain Mueller <mueller@tplxdt026.nat.physics.ox.ac.uk>
2017-07-26 22:12:20 +0100

    inverted arguments

--- wait.sh
+++ wait.sh
@@ -2,21 +2,21 @@
 
 # Wait for the number of jobs in a node to be less than a certain number. Usage:
 #
-#    ./wait name N
+#    ./wait N name
 #
 # Checks every 5 seconds that the number of subprocesses running on node name is
 # greater than N and that the global lock is free. If this is not the case, then
 # returns.
 
 # if N is set to 0 or empty
-if [ $# -le 2 ] || [ $2 == "0" ]; then
+if [ $1 == "0" ]; then
   exit 0;
 fi
 
 # get all sub jobs with that name (i.e. name followed by .number)
-read -a PIDS <<< $( ${MJM_PATH}/list.sh $1 )
+read -a PIDS <<< $( ${MJM_PATH}/list.sh $2 )
 
-while [ ${#PIDS[@]} -ge $2 ]; do
+while [ ${#PIDS[@]} -ge $1 ]; do
   sleep 5
-  read -a PIDS <<< $( ${MJM_PATH}/list.sh )
+  read -a PIDS <<< $( ${MJM_PATH}/list.sh $2 )
 done
