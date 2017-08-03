8bbb99086acda3c3697bfd61fab6394304eaff18
Romain Mueller <romain.mueller@gmail.com>
2017-08-03 19:03:22 +0100

    pid now gives real pid back

--- cmds/pid.sh
+++ cmds/pid.sh
@@ -4,7 +4,8 @@ PIDS=""
 
 for i in "$@"
 do
-  PID=$( screen -ls | awk "/${i}.*\t/ {print \$1}" )
+  PID=$( screen -ls | awk "/$i.*\t/ {print \$1}" )
+  PID=${PID%.$i}
 
   PIDS="${PIDS[@]}$PID "
 done
